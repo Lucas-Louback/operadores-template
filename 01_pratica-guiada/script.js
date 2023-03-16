e0b6d7a339790ecf5b5d777cb9be880e6ba55340
Lucas-Louback <lucasmlouback99@gmail.com>
2023-03-16 12:59:17 -0300

    atividade operadores template pronta

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,55 @@
-//CODIGO AQUI
+//Exercício
+//Depois fazer por códigos
+console.log("Prática 1")
+
+let saldo = 15
+console.log("o saldo inicial é de: ", saldo)
+
+saldo = saldo + 22.40
+// poderia ser usado saldo += 22.40
+console.log("Foram adicionados 22.40 saldo inicial, o novo saldo ficou em: ", saldo)
+
+saldo -= 12.34
+//poderia ser usado saldo = saldo - 12.34
+console.log("Foram debitados 22.40 saldo inicial, o novo saldo ficou em: ", saldo)
+
+saldo = saldo / 2
+console.log("o saldo foi dividido pela metade e passou a ser: ", saldo)
+
+saldo = saldo * 3.40
+console.log("o saldo foi multiplicado por 3.4 e passou a ser: ", saldo.toFixed(2) )
+
+saldo = saldo % 3
+console.log("saldo foi dividido por 3 e o resto foi: ", saldo.toFixed(2) )
+
+let mult = 2 + 2 * 2
+console.log(mult)
+
+let mult2 = 2*(2+2)
+console.log(mult2)
+
+// EXERCICIO DE FIXAÇÃO
+console.log(5===5)
+console.log(5!==5)
+console.log(5!=="5")
+console.log("5"==="cinco")
+console.log(typeof 5 === typeof 20)
+console.log(typeof "5" === typeof "cinco")
+
+console.log("pratica 3")
+
+let x = 20
+let y = 30
+
+console.log(`Xé igual a ${x} e Y é igual a ${y}`)
+console.log(`X é igual a Y? ${x===y}`)
+console.log(`X é menor ou igual a Y? ${x<=y}`)
+console.log(`X é maior ou igual a Y? ${x>=y}`)
+console.log(`X é maior a Y? ${x > y}`)
+console.log(`X é menor a Y? ${x < y}`)
+
+
+
+
+
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,8 @@
 // EXERCICIO DE FIXAÇÃO
-
+let idade = Number( prompt("Qual a sua idade? ") )
+console.log(`A sua idade é ${idade}`)
+let idadeAmigo = Number( prompt(`Qual a idade do seu melhor amigo(a)? `) )
+console.log(`A idade do(a) seu(sua) amigo(a) é ${idadeAmigo}`)
+console.log(`A idade do seu(sua) amigo(a) é maior ou à sua? ${idade >= idadeAmigo}`)
+console.log(`A idade do seu(sua) amigo(a) é igual à sua? ${idade === idadeAmigo}`)
+console.log(`a diferença de idade entre vocês é de ${idade - idadeAmigo} anos`)
